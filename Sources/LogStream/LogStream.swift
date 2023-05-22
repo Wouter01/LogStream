71c743b55be0161cd51ac4585305bbf2b22c75d4
Wouter01 <wouterhennen@gmail.com>
2023-05-22 16:25:28 +0200

    Added function for multiple processes
    Signed-off-by: Wouter01 <wouterhennen@gmail.com>

--- Sources/LogStream/LogStream.swift
+++ Sources/LogStream/LogStream.swift
@@ -12,7 +12,7 @@ public enum LogStream {
     /// Retrieve activity logs for a specific process identifier (PID) using an asynchronous stream.
     ///
     /// - Parameters:
-    ///   - pid: The process identifier (PID) of the target process.
+    ///   - processID: The process identifier (PID) of the target process.
     ///   - flags: The options specifying the behavior of the activity stream. Default value is `[.historical, .processOnly]`.
     /// - Returns: An `AsyncStream` that emits `LogMessage` objects representing the activity logs.
     ///
@@ -24,42 +24,51 @@ public enum LogStream {
     ///
     /// Task {
     ///     for await log in logStream {
-    ///         print(log)
+    ///         print("\(log.process) says:", log.message)
     ///     }
     /// }
     /// ```
-    public static func logs(for pid: pid_t, flags: ActivityStreamOptions = [.historical, .processOnly]) -> AsyncStream<LogMessage> {
-        return AsyncStream { cont in
+    public static func logs(for processID: pid_t, flags: ActivityStreamOptions = [.historical, .processOnly]) -> AsyncStream<LogMessage> {
+        return AsyncStream { continuation in
 
-            let stream = streamLog(pid: pid, flags: flags.rawValue) { entryPointer, error in
+            let stream = createStream(pid: processID, flags: flags, continuation: continuation)
 
-                guard error == 0, let entryPointer else { return false }
-
-                let entry = entryPointer.pointee
-
-                guard entry.type == OS_ACTIVITY_STREAM_TYPE_LOG_MESSAGE || entry.type == OS_ACTIVITY_STREAM_TYPE_LEGACY_LOG_MESSAGE else { return true }
-
-                let event = OSActivityLogMessageEvent(entry: entryPointer)
-
-                let logMessage = LogMessage(
-                    message: event.eventMessage,
-                    date: event.timestamp,
-                    subsystem: event.subsystem,
-                    category: event.category,
-                    type: .init(event.messageType),
-                    process: event.process,
-                    processID: event.processID
-                )
-                
-                cont.yield(logMessage)
-                return true
-            }
-
-            cont.onTermination = { _ in
+            continuation.onTermination = { _ in
                 cancelLog(stream: stream)
             }
 
             resumeLog(stream: stream)
+        }
+    }
+
+    /// Retrieve activity logs for a selection of processes using an asynchronous stream.
+    ///
+    /// - Parameters:
+    ///   - processIDs: A list of process identifiers (PID) of the desired target processes.
+    ///   - flags: The options specifying the behavior of the activity stream. Default value is `[.historical, .processOnly]`.
+    /// - Returns: An `AsyncStream` that emits `LogMessage` objects representing the activity logs.
+    ///
+    /// Usage Example:
+    ///
+    /// ```swift
+    /// Task {
+    ///     let pids: [pid_t] = ... // Specify the process identifiers
+    ///     for await log in LogStream.logs(for: pids) {
+    ///         print("\(log.process) says:", log.message)
+    ///     }
+    /// }
+    /// ```
+    public static func logs(for processIDs: [pid_t], flags: ActivityStreamOptions = [.historical, .processOnly]) -> AsyncStream<LogMessage> {
+        AsyncStream { continuation in
+            let streams = processIDs.map {
+                createStream(pid: $0, flags: flags, continuation: continuation)
+            }
+
+            continuation.onTermination = { _ in
+                streams.forEach(cancelLog)
+            }
+
+            streams.forEach(resumeLog)
         }
     }
 
@@ -73,13 +82,39 @@ public enum LogStream {
     ///
     /// ```swift
     /// Task {
-    ///     for await log in LogStream.logs() {
-    ///         print(log)
+    ///     for await log in LogStream.logs(for: ) {
+    ///         print("\(log.process) says:", log.message)
     ///     }
     /// }
     /// ```
     public static func logs(flags: ActivityStreamOptions = [.historical, .processOnly]) -> AsyncStream<LogMessage> {
         LogStream.logs(for: -1, flags: flags)
+    }
+
+    static func createStream(pid: pid_t, flags: ActivityStreamOptions, continuation: AsyncStream<LogMessage>.Continuation) -> ActivityStream? {
+        streamLog(pid: pid, flags: flags.rawValue) { entryPointer, error in
+
+            guard error == 0, let entryPointer else { return false }
+
+            let entry = entryPointer.pointee
+
+            guard entry.type == OS_ACTIVITY_STREAM_TYPE_LOG_MESSAGE || entry.type == OS_ACTIVITY_STREAM_TYPE_LEGACY_LOG_MESSAGE else { return true }
+
+            let event = OSActivityLogMessageEvent(entry: entryPointer)
+
+            let logMessage = LogMessage(
+                message: event.eventMessage,
+                date: event.timestamp,
+                subsystem: event.subsystem,
+                category: event.category,
+                type: .init(event.messageType),
+                process: event.process,
+                processID: event.processID
+            )
+
+            continuation.yield(logMessage)
+            return true
+        }
     }
 }
 
